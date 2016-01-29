c23648f676c1b35ce6933ac95308e6555bcae650
Akira Chiku <akira.chiku@gmail.com>
2016-01-29 13:57:13 +0900

    Clean up config

--- install-nginx-build.sh
+++ install-nginx-build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 VERSION=0.6.8
+DOWNLOAD_URL=https://github.com/cubicdaiya/nginx-build/releases/download
 
 if [[ ! -d ./bin ]]; then 
     mkdir ./bin
 fi
-wget https://github.com/cubicdaiya/nginx-build/releases/download/v${VERSION}/nginx-build-darwin-amd64-${VERSION}.tar.gz -O ./bin/${VERSION}.tar.gz
+
+wget ${DOWNLOAD_URL}/v${VERSION}/nginx-build-darwin-amd64-${VERSION}.tar.gz -O ./bin/${VERSION}.tar.gz
 (
     cd ./bin
     tar xvfz ${VERSION}.tar.gz
